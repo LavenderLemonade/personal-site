2502d0351a1994bc90155c56d41d7f049dfef486
Sam Elliott <soe1@uakron.edu>
2022-10-23 18:21:42 -0400

    we're finally getting somewhere, got that whole mouseover stuff on lock

--- secondscript.js
+++ secondscript.js
@@ -2,11 +2,11 @@ document.getElementById("my-name").innerHTML = " Changed Up!";
 
 document.getElementsByClassName("in-there")[0].innerHTML = "Boo!";
 
-function MouseOver(){
-    document.getElementsByClassName("in-there")[0].style.color = "red";
+function MouseOver(thing){
+    thing.style.color = "red";
 }
 
-function MouseOut(){
-    document.getElementsByClassName("in-there")[0].style.color = "black";
+function MouseOut(thing){
+    thing.style.color = "black";
 }
 
